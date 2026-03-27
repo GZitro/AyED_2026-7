c88efa030d38ea5d61ecae5681fc14e67eb8c261
GZitro <gonzaloortizponte09@gmail.com>
2026-03-27 16:35:33 -0300

    Add files via upload

---
+++ 7_ortiz_M/tp 3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_1_gonza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Hola usuario");
+            Console.Write("¿cual es tu año de nacimiento?: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+
+            int edad = 2026 - año;
+            Console.WriteLine("Si naciste en el año "+ año+ " tu edad al comenzar el año es de "+ edad+ " años.");
+        }
+    }
+}
