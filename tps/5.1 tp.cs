0266053476878386f873f7346ef7609c8941cd7a
GZitro <gonzaloortizponte09@gmail.com>
2026-04-08 16:58:17 -0300

    Add files via upload

---
+++ tps/5.1 tp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("cuanto es el nivel midiclorianos?: ");
+
+            int poder = Convert.ToInt32(Console.ReadLine());
+
+            if (poder >= 15000)
+            {
+
+                Console.WriteLine("potencial maestrojedi detectado. avisa al consejo");
+
+            }
+            if (5000 < poder)
+            {
+                if (poder<15000)
+                    { 
+                Console.WriteLine("Aceptable para entrenamiento Padawan.");
+                     }
+                }
+            else
+            {
+                Console.WriteLine("No posee sensibilidad a la Fuerza");
+
+            }
+        }
+
+    }
+}
+
+        
---
+++ tps/5.4 tp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("cuantos droides vas a comprar?: ");
+
+            int droides = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            int cantidad = droides * 2000;
+            
+                if (cantidad >= 6000)
+            {
+                int iva = cantidad * 20 / 100;
+                int preciof = cantidad-iva;
+                Console.WriteLine(iva+ " descuento");
+                Console.WriteLine(preciof + " precio final");
+            }
+          
+            else
+            {
+                Console.WriteLine(cantidad + " precio final");
+               
+            }
+        }
+
+    }
+}
+
+        
