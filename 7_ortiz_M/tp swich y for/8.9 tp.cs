57107f4eedfaf35421f266fea34d50e55c00c113
GZitro <gonzaloortizponte09@gmail.com>
2026-05-07 15:14:45 -0300

    Add files via upload

---
+++ 7_ortiz_M/tp swich y for/8.9 tp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el gasto de los ultimos 5 dias");
+
+            int acumulador = 0;
+            int contador = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("dia " + i + " cuanto gasto? ");
+
+                int plata = Convert.ToInt32(Console.ReadLine());
+
+                acumulador = acumulador + plata;
+
+                if (plata > 500)
+                {
+                    contador++;
+
+                }
+            }
+            Console.WriteLine(acumulador + " gasto en total");
+            Console.WriteLine(contador + " dia mas de 500");
+        }
+    }
+}
