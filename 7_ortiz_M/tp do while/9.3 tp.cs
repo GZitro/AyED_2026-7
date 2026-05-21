21f9fb47f76c758cb312b9c1b209ac905d24022c
GZitro <gonzaloortizponte09@gmail.com>
2026-05-21 13:57:31 -0300

    Add files via upload

---
+++ 7_ortiz_M/tp do while/9.3 tp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ingrese su dinero a la alcancia");
+            int plata;
+            int alcancia = 0;
+
+            do
+            {
+                Console.Write("cuanto genero ");
+               plata = Convert.ToInt32(Console.ReadLine());
+                
+
+                alcancia = alcancia + plata;
+               
+
+
+
+
+            } while (plata > 0);
+
+
+            Console.WriteLine("tu dinero final es " + alcancia);
+        }
+    }
+}
+
+               
---
+++ 7_ortiz_M/tp do while/92.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su nota");
+            int nota;
+            do
+            {
+                nota = Convert.ToInt32(Console.ReadLine());
+
+                if (nota <= 10 && nota >= 0)
+                {
+
+                    Console.WriteLine("su nota es " + nota);
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+
+            } while (nota > 10 && nota > 0);
+        }
+
+    }
+}
