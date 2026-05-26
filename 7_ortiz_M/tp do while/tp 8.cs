ca71567b73c6ad38293660aad235b590069ba61b
GZitro <gonzaloortizponte09@gmail.com>
2026-05-26 14:04:24 -0300

    Add files via upload

---
+++ 7_ortiz_M/tp do while/tp 8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" 1_ Saludar.");
+            Console.WriteLine(" 2_ Despedirse");
+            Console.WriteLine(" 3_ Salir.");
+            int opcion;
+            do
+            {
+                Console.Write("elija la opcion que quiera: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                if (opcion == 1)
+                {
+                    Console.WriteLine("HOLA MUNDO");
+                }
+                if (opcion == 2)
+                {
+                    Console.WriteLine("CHAU MUNDO");
+
+                }
+            } while (opcion != 3);
+        }
+    }
+}
