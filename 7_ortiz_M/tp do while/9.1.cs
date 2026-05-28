09015c0af861d9741c52aec8d8704ccd577e4d95
GZitro <gonzaloortizponte09@gmail.com>
2026-05-28 14:40:21 -0300

    Add files via upload

---
+++ 7_ortiz_M/tp do while/9.1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ingrese su numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+
+                Console.WriteLine("numero " + num);
+                num--;
+            } while (num >= 0);
+        }
+    }
+}
