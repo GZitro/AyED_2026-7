68c64305cf157e6ad8e21ab6f2ba9e0e2a79bf38
GZitro <gonzaloortizponte09@gmail.com>
2026-08-11 16:57:32 -0300

    Add files via upload

---
+++ 7_ortiz_M/tps arives/tp notas.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nt;
+            int[] notas = new int[10];
+            int contador = 0;
+            int aprob = 0;
+            int desaprob = 0;
+
+
+
+         
+            for (int i = 0; i < notas.Length; i++)
+            {
+
+                Console.Write("Ingrese la nota de los alumnos " + (i+1) + " : ");
+
+                nt = Convert.ToInt32(Console.ReadLine());
+                if (nt < 10)
+                {
+               notas[i] = nt;
+                }
+               else
+                {
+                    Console.WriteLine("Ingrese un valor menor a 10");
+                    i--;
+                }
+            }
+
+
+
+
+
+
+            foreach (int suma in notas)
+            {
+                contador = contador + suma;
+                if (suma>=6)
+                {
+                    aprob++;
+                }
+                else
+                {
+                    desaprob++;
+                }
+            }
+
+            double promedio = contador / notas.Length;
+            Console.WriteLine("el promedio de las notas es: " + promedio);
+            Console.WriteLine("aprobaron: " + aprob);
+            Console.WriteLine("desaprobaron: " + desaprob);
+
+
+            //for (int i = 0; i < temperatura.Length; i++)
+            //{
+
+            //    Console.WriteLine(temperatura[i]);
+
+            //}
+
+
+
+
+            //Un docente tiene un curso de 10 alumnos.El programa debe permitir cargar las 10 notas(valores de 1 a 10) y
+            //luego mostrar cuántos alumnos aprobaron (nota mayor o igual a 6), cuántos desaprobaron, y el promedio
+            //general del curso.
+
+
+        }
+    }
+}
+
+
---
+++ 7_ortiz_M/tps arives/tp temp.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            int[] temperatura = new int[7];
+            int contador = 0;
+            int mayor = 0;
+
+
+
+
+            string[] dias = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes","sabado","domingo"};
+            for (int i = 0; i < temperatura.Length; i++)
+            {
+
+                Console.Write("Ingrese la temperaura del dia "+dias[i]+" : ");
+
+                temp = Convert.ToInt32(Console.ReadLine());
+                temperatura[i] = temp;
+            }
+            int menor = temperatura[0];
+
+            foreach (int suma in temperatura)
+            {
+
+                contador = contador + suma;
+                if (suma > mayor)
+                {
+                    mayor = suma;
+                }
+                if (suma < menor)
+                {
+                    menor = suma;
+                }
+
+            }
+
+            int promedio = contador / temperatura.Length;
+                Console.WriteLine("el promedio de la semana fue "+promedio);
+                Console.WriteLine("la mayor temperatura fue de "+mayor);
+                Console.WriteLine("la menor temperatura fue de "+menor);
+
+
+
+
+            //for (int i = 0; i < temperatura.Length; i++)
+            //{
+
+            //    Console.WriteLine(temperatura[i]);
+
+            //}
+
+
+
+
+
+
+
+
+
+            //Un centro meteorológico registra la temperatura máxima de cada día de la semana(7 valores).El programa
+            //debe permitir cargar las 7 temperaturas y luego mostrar: la temperatura más alta de la semana, la más baja, y el
+            //promedio semana
+        }
+        }
+    }
+
