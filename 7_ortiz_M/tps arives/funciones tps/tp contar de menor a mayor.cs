13aff806bffd0d2f15e882d597faf2b4e283d285
GZitro <gonzaloortizponte09@gmail.com>
2026-08-25 16:56:33 -0300

    Add files via upload

---
+++ 7_ortiz_M/tps arives/funciones tps/tp contar de menor a mayor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ContarHasta
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContarHasta();
+        }
+        static void ContarHasta()
+        {
+            int contador = 1;
+            int num;
+            Console.Write("Ingrese un numero:");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (contador <= num)
+            {
+                
+                Console.WriteLine(contador);
+                contador++;
+            }
+        }
+    }
+}
+//2. Desarrollar una función llamada ContarHasta que 
+//    reciba un número entero positivo y muestre por pantalla todos 
+//    los números desde 1 hasta ese número.
