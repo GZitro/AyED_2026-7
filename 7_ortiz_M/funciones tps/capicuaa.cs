4a43c72c0072e823af4b96e8ca21c87cae07ff8a
GZitro <gonzaloortizponte09@gmail.com>
2026-09-15 16:58:35 -0300

    Add files via upload

---
+++ 7_ortiz_M/funciones tps/capicuaa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Capicua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EsCapicua();
+        }
+
+        static void EsCapicua()
+        {
+            Console.Write("ingrese un numero entero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int numero = num;
+            int numero0 = 0;
+            int digito;
+
+            while (numero > 0)
+            {
+                digito = numero % 10;
+                numero0 = (numero0 * 10) + digito;
+                numero = numero / 10;
+            }
+
+            if (num == numero0)
+            {
+                Console.WriteLine("El numero " + num + " es capicua");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + num + " no es capicua");
+            }
+        }
+    }
+}
---
+++ 7_ortiz_M/funciones tps/prueba.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double limite =6247642549258932538 ; 
+
+            for (double i = 1; i <= limite; i++)
+            {
+                double numeroActual = i;
+                Console.WriteLine($"\n--- Secuencia para el número {i} ---");
+
+                while (numeroActual != 1)
+                {
+                    if (numeroActual % 2 == 0)
+                    {
+                        numeroActual = numeroActual / 2;
+                    }
+                    else
+                    {
+                        numeroActual = numeroActual * 3 + 1;
+                    }
+                    Console.WriteLine(numeroActual);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
